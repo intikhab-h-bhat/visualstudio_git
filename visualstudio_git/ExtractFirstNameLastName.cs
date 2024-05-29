f00357796c6c7563acf25b6c51a64074fc650560
intikhab-h-bhat <44907729+intikhab-h-bhat@users.noreply.github.com>
2024-05-29 21:56:31 +0530

    added new class (#1)
    * intial commit
    
    * added new class

---
+++ visualstudio_git/ExtractFirstNameLastName.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace visualstudio_git
+{
+    internal class ExtractFirstNameLastName
+    {
+        private string _fullName;
+        private string _firstName;
+        private string _lastName;
+
+        public string FullName { get => _fullName; set => _fullName = value; }
+        public string FirstName { get => _firstName; set => _firstName = value; }
+        public string LastName { get => _lastName; set => _lastName = value; }
+        
+        public string getFirstNameLastName()
+        {
+            return _firstName;
+        }
+       
+
+    }
+}
