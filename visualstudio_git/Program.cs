85f4b9803da98eb5e33bb89e49ee290643c12e34
intikhab-h-bhat <intikhabh@gmail.com>
2024-05-31 11:03:59 +0530

    changes in programm.cs file

--- visualstudio_git/Program.cs
+++ visualstudio_git/Program.cs
@@ -3,7 +3,10 @@ using visualstudio_git;
 
 Console.WriteLine("Welcome to ABC Bank");
 Console.WriteLine("_________________________________________");
+Console.WriteLine("_________________________________________\n");
 
+Console.WriteLine("Customer Details");
+Console.WriteLine("_________________________________________");
 
 var customer1 = new Customer("Intikhab Hussain","Bhat",43,"intikhabh@gmail.com","Nowgam By-Pass Srinagar",8491021591,1);
 customer1.GetCustomerInfo();
@@ -15,6 +18,7 @@ acountdetails.GetAccountDetails();
 
 Console.WriteLine("\n");
 Console.WriteLine("Transaction Details");
+Console.WriteLine("_________________________________________");
 var transDetails = new Transactions(acountdetails.GetAcountNo(),-500,DateTime.Now,"Withdrawl");
 
 //Console.WriteLine($"Amount in Bank : {acountdetails.GetBalance()}");
@@ -27,6 +31,7 @@ Console.WriteLine($"Balance in Bank : {acountdetails.GetBalance()}");
 
 Console.WriteLine("\n");
 Console.WriteLine("Employee Details");
+Console.WriteLine("_________________________________________");
 var empDetails = new Employee("Javaid","Ahmad",38,"abc@gmail.com","Natipora Srinagar",1234567890,101,"Accountant");
 
 empDetails.GetEmployeeInfo();
